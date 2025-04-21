31da89803e66d54555e61a1adce772599c7c694c
Akzhol71 <muratuly.19@gmail.com>
2025-04-21 21:27:14 +0500

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 // 👇 ВСТАВЬ СЮДА свой actual Google Script URL:
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyJOoaLJEA8NykMEGmc8fJ45CuiGYeDAimSqddLUh2_GGUPod8otfrXK6t9XyffxZpmbg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxTsS8QZKDhr3zeP3AUGIjb82wPdWjpUCTX-iVcFqRmEFLGP0VWKqEGKI_IQFPEylJCtw/exec";
 
 const events = [
   {
